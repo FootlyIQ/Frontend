7af983abc6d62d9d06e2cdd2f4ec2f752723d02b
DvorsakGal <gal.dvorsak2002@gmail.com>
2025-05-13 10:58:13 +0200

    test povezave z be + axios

--- src/components/Home/MatchFeed.jsx
+++ src/components/Home/MatchFeed.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
 export default function MatchFeed() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+      const fetchTest = async () => {
+        try {
+          const response = await axios.get("http://127.0.0.1:5000/test");
+          setMessage(response.data);
+        } catch (error) {
+          console.error("Error fetching test data from backend: ", error);
+        }
+      };
+
+      fetchTest();
+    }, []);
+
     return (
       <section>
         <h2 className="text-xl font-bold mb-4 text-gray-800">Today's Matches</h2>
@@ -9,6 +28,10 @@ export default function MatchFeed() {
               <p className="text-sm text-gray-500">Kickoff: 20:45 CET</p>
             </div>
           ))}
+        </div>
+        <div>
+          <h3>Test:</h3>
+          <p>{message}</p>
         </div>
       </section>
     );
