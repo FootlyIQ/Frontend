015dc0fac105bdf41454c15e424a941fe3de5d97
DvorsakGal <gal.dvorsak2002@gmail.com>
2025-05-20 17:07:31 +0200

    added a pitch to xG tab and removed some unnecesarry npm installs

--- src/components/AnalysisHub/XG.jsx
+++ src/components/AnalysisHub/XG.jsx
@@ -1,8 +1,9 @@
+import FootballPitchXG from "./FootballPitchXG";
+
 export default function XG() {
     return (
-        <div>
-            <h3>xG Analysis</h3>
-            <p>This is the expected goals analysis section.</p>
-        </div>
+        <>
+        <FootballPitchXG />
+        </>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import RightSidebar from "../components/Home/RightSidebar";
 export default function Home() {
 
     return (
+        
         <div className="flex flex-col lg:flex-row min-h-screen bg-slate-800 text-gray-900">
             
             <main className="flex-1 p-6 bg-slate-800">
